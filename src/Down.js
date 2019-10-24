5aabc588e9d3de3430689abb5ae4941ac3b6faf4
jonam <jadhikari131@gmail.com>
2019-10-24 21:31:46 +0900

    all file is edit

---
+++ src/Down.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+function Down(){
+	return(
+			<div>
+				<div class="row mx-md-n5 navbar navbar-expand-lg navbar-light bg-light navbar-fixed-bottom">
+				  <div class="col px-md-5" >
+				  	<div>
+				  		Luggage list
+				  	</div>
+				  </div>
+				  <div class="col px-md-5" align="right">
+				  	<div >
+				  			New
+				  	</div>
+				  </div>
+				</div>
+			</div>
+		)
+}
+export default Down;
---
+++ src/Top.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './top.css';
+
+function Top(){
+	return(
+			<div>
+				<div class="row mx-md-n5 navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top">
+				  <div class="col px-md-5" >
+				  	<div>
+				  		Luggage list
+				  	</div>
+				  </div>
+				  <div class="col px-md-5" align="right" >
+				  	<div >
+				  			New
+				  	</div>
+				  </div>
+				</div>
+			</div>
+		)
+}
+export default Top;
